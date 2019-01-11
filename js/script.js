6ffea94775c25c6521c0ac4ee6d30f168a0f1880
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-11 07:43:56 +0100

    Add titel, table head, table body and table components

---
+++ js/script.js
@@ -0,0 +1,98 @@
+class TableHead extends React.Component {
+	render() {
+		return (
+			<thead>
+				<tr>
+					<td>nazwa</td>
+					<td>rodzaj</td>
+					<td>alk</td>
+					<td>producent</td>
+					<td>butelka</td>
+				</tr>
+			</thead>
+		)
+	}
+}
+
+class TableBody extends React.Component {
+
+	render() {
+		return (
+			<tbody>
+			{this.props.beers.map( beer => 
+				<tr>
+					<td>{beer.name}</td>
+					<td>{beer.art}</td>
+					<td>{beer.alk}</td>
+					<td>{beer.manufacturer}</td>
+					<td><img src={beer.img}></img></td>
+				</tr>
+			)}
+			</tbody>
+		)
+	}
+}
+
+class Table extends React.Component {
+
+	beers = [
+				{
+					name: 'ciechan miodowe',
+					art: 'jasne',
+					alk: '5.7%',
+					manufacturer: 'browar ciechan',
+					img: 'img/ciechan-miodowe.png'
+				},
+				{
+					name: 'łomża miodowe',
+					art: 'jasne',
+					alk: '5.7%',
+					manufacturer: 'browar łomża',
+					img: 'img/lomza-miodowe.png'
+				},
+				{
+					name: 'perła miodowe',
+					art: 'jasne',
+					alk: '6%',
+					manufacturer: 'browary lubuskie',
+					img: 'img/perla-miodowa.png'
+				}
+			]
+
+	render() {
+		return (
+			<table>
+				<TableHead />
+				<TableBody beers = {this.beers}/>
+			</table>
+		)
+	}
+}
+
+
+class Title extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+			</div>
+		)
+	}
+}
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<Title title = 'lista piw miodowych'/>
+				<Table />
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App/>,
+	document.getElementById('app')
+)
